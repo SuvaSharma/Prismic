3400e0370d3722e998880f687b42a2622f14cd51
SuvaSharma <110406101+SuvaSharma@users.noreply.github.com>
2024-12-08 20:34:10 +0545

    view analytics for vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,8 +6,9 @@ import Footer from "@/components/Footer";
 import clsx from "clsx";
 import { PrismicPreview } from "@prismicio/next";
 import { createClient, repositoryName } from "@/prismicio";
+import { Analytics } from "@vercel/analytics/react";  // Import the Analytics component
 
-const urbanist = Urbanist({ subsets: ['latin']})
+const urbanist = Urbanist({ subsets: ['latin'] })
 
 export async function generateMetadata(): Promise<Metadata> {
   const client = createClient();
@@ -17,7 +18,6 @@ export async function generateMetadata(): Promise<Metadata> {
     title: settings.data.meta_data,
     description: settings.data.meta_description,
   };
-  
 }
 
 export default function RootLayout({
@@ -27,18 +27,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="bg-[#C6E7FF] text-[#ffffff]">
-    
-      <body
-        className={clsx(urbanist.className, "relative min-h-screen")}
-      >
-          <Header />
+      <body className={clsx(urbanist.className, "relative min-h-screen")}>
+        <Header />
         {children}
         <Footer />
         <div className="absolute inset-0 -z-50 max-h-screen background-gradient"></div>
-        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-50 mix-blend-soft-light">
-        </div>
+        <div className="absolute pointer-events-none inset-0 -z-40 h-full bg-[url('/noisetexture.jpg')] opacity-50 mix-blend-soft-light"></div>
+
+        {/* Add the Analytics component here */}
+        <Analytics />
       </body>
-      <PrismicPreview repositoryName= {repositoryName} />
+      <PrismicPreview repositoryName={repositoryName} />
     </html>
   );
 }
